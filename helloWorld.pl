fb1978a2655bb3fc8a2b9eaae8cd122dd1143746
markf <markfitz815@gmail.com>
2022-06-27 20:19:35 +0100

    Work with loops, arrays and hashes

--- helloWorld.pl
+++ helloWorld.pl
@@ -7,7 +7,18 @@ $c = $a . "2"; # This will be 52 due to periods being concatonated
 
 @dbzCharacters = ("Goku", "Vegeta", "Trunks"); # This is an Array
 
+%car = ( "Wheels" => "4", "Color" => "Blue", "Engine" => "1.6" ); # This is a Hash (AKA Dictionary or Object)
+
+@car_keys = keys %car;
+
 print $message;
 print $b, "\n";
 print $c, "\n";
 print $dbzCharacters[0], "\n"; # Selecting an Array scalar the dollar sign must be used
+print $#dbzCharacters, "\n"; # Gives the length of an array (equals the actual length -1)
+print $car{"Color"}, "\n"; # Retrive an a value from a hash
+
+# This is a for loop
+for $i (@car_keys) {
+    print $i, "\n";
+}
