cd9086c7d9ea3a37be7c9da8014eca3b8e8e547d
markf <markfitz815@gmail.com>
2022-06-27 19:55:21 +0100

    Initial helloWorld file

---
+++ helloWorld.pl
@@ -0,0 +1,13 @@
+
+$message = "My World, my Darling. \n";
+
+$a = "5"; # Number uses commas
+$b = $a + "2"; # This will be 7
+$c = $a . "2"; # This will be 52 due to periods being concatonated
+
+@dbzCharacters = ("Goku", "Vegeta", "Trunks"); # This is an Array
+
+print $message;
+print $b, "\n";
+print $c, "\n";
+print $dbzCharacters[0], "\n"; # Selecting an Array scalar the dollar sign must be used
